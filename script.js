8fe457ec58c404f6ebec489c9c1cf5447e9b669c
Richard Lima <112436267+RichiLima@users.noreply.github.com>
2023-01-18 20:51:38 -0300

    atualização do portifolio

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-var menu = window.document.getElementById(menu-fixed)
+//############## Barra de navegação ########
+const menu = document.getElementById('menu-fixed');
 
+// ##### iniciando função de estilizar navbar ######
+window.onscroll = function(){scrollNav()} 
+
+function scrollNav(){
+    var positionX = document.documentElement.scrollTop;
+    
+    if(positionX >= 50){
+        menu.classList.add('scroll__active')
+    }else{
+        menu.classList.remove('scroll__active')
+    }
+}
+
+// ########### fim da função ###############
